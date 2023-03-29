fda29cd320b769a5948c04cbf4a314d504161d7f
Iara Varjao Barbosa <103008910+iaravarjao@users.noreply.github.com>
2023-03-29 00:44:36 -0300

    Update script.js

--- superCalculadora/script.js
+++ superCalculadora/script.js
@@ -1,3 +1,69 @@
+const soma = document.getElementById('soma');
+const subtracao1 = document.getElementById('subtracao1');
+const subtracao2 = document.getElementById('subtracao2');
+const divisao1 = document.getElementById('divisao1');
+const divisao2 = document.getElementById('divisao2');
+const potencia1 = document.getElementById('potencia1');
+const potencia2 = document.getElementById('potencia2');
+const raiza = document.getElementById('raiza');
+const raizb = document.getElementById('raizb');
+const porcent1 = document.getElementById('porcent1');
+const porcent2 = document.getElementById('porcent2');
+const media = document.getElementById('media');
+const fatA = document.getElementById('fatA');
+const fatB = document.getElementById('fatB');
+
+soma.innerHTML = 0;
+subtracao1.innerHTML = 0;
+subtracao2.innerHTML = 0;
+divisao1.innerHTML = 0;
+divisao2.innerHTML = 0;
+potencia1.innerHTML = 0;
+potencia2.innerHTML = 0;
+raiza.innerHTML = 0;
+raizb.innerHTML = 0;
+porcent1.innerHTML = 0;
+porcent2.innerHTML = 0;
+media.innerHTML = 0;
+fatA.innerHTML = 0;
+fatB.innerHTML = 0;
+
+function calcularSoma(x, y) {
+  return x + y;
+}
+function calcularSubtracao1(x, y) {
+  return x - y;
+}
+function calcularSubtracao2(x, y) {
+  return y - x;
+}
+function calcularDivisao1(x, y) {
+  return x / y;
+}
+function calcularDivisao2(x, y) {
+  return y / x;
+}
+function calcularPotencia1(x, y) {
+  return Math.pow(x, y);
+}
+function calcularPotencia2(x, y) {
+  return Math.pow(y, x);
+}
+function calcularRaiz1(x) {
+  return Math.sqrt(x).toFixed(2);
+}
+function calcularRaiz2(y) {
+  return Math.sqrt(y).toFixed(2);
+}
+function calcularPorcent1(x, y) {
+  return ((x * 100) / y).toFixed(2);
+}
+function calcularPorcent2(x, y) {
+  return ((y * 100) / x).toFixed(2);
+}
+function calcularMedia(x, y) {
+  return (x + y) / 2;
+}
 function fatorialA(na) {
   if (na === 0 || na === 1) return 1;
   for (var i = na - 1; i >= 1; i--) {
@@ -13,52 +79,22 @@ function fatorialB(nb) {
   return nb;
 }
 
-function Calcular(A, B) {
-  var A = Number(document.getElementById('A').value);
-  var B = Number(document.getElementById('B').value);
-  var soma = A + B;
-  var subtracao1 = A - B;
-  var subtracao2 = B - A;
-  var divisao1 = (A / B).toFixed(2);
-  var divisao2 = (B / A).toFixed(2);
-  var potencia1 = A ** B;
-  var potencia2 = B ** A;
-  var raiza = Math.sqrt(A).toFixed(2);
-  var raizb = Math.sqrt(B).toFixed(2);
-  var porcent1 = ((A * 100) / B).toFixed(2);
-  var porcent2 = ((B * 100) / A).toFixed(2);
-  var media = (A + B) / 2;
-  var fatA = fatorialA(A);
-  var fatB = fatorialB(B);
+function Calcular() {
+  const A = Number(document.getElementById('A').value);
+  const B = Number(document.getElementById('B').value);
 
-  alert(
-    'Soma: ' +
-      soma +
-      '    Substração A-B: ' +
-      subtracao1 +
-      '    Substração B-A: ' +
-      subtracao2 +
-      '    Divisão A/B: ' +
-      divisao1 +
-      '    Divisão B/A: ' +
-      divisao2 +
-      '    Potenciação A^B: ' +
-      potencia1 +
-      '    Potenciação B^A: ' +
-      potencia2 +
-      '    Raiz Quadrada A: ' +
-      raiza +
-      '    Raiz Quadrada B: ' +
-      raizb +
-      '    Porcentagem A rel B: ' +
-      porcent1 +
-      '    Porcentagem B rel a: ' +
-      porcent2 +
-      '    Média: ' +
-      media +
-      '    Fatorial de A: ' +
-      fatA +
-      '    Fatorial de B: ' +
-      fatB
-  );
+  soma.innerHTML = calcularSoma(A, B);
+  subtracao1.innerHTML = calcularSubtracao1(A, B);
+  subtracao2.innerHTML = calcularSubtracao2(A, B);
+  divisao1.innerHTML = calcularDivisao1(A, B);
+  divisao2.innerHTML = calcularDivisao2(A, B);
+  potencia1.innerHTML = calcularPotencia1(A, B);
+  potencia2.innerHTML = calcularPotencia2(A, B);
+  raiza.innerHTML = calcularRaiz1(A);
+  raizb.innerHTML = calcularRaiz2(B);
+  porcent1.innerHTML = calcularPorcent1(A, B);
+  porcent2.innerHTML = calcularPorcent2(A, B);
+  media.innerHTML = calcularMedia(A, B);
+  fatA.innerHTML = fatorialA(A);
+  fatB.innerHTML = fatorialB(B);
 }
